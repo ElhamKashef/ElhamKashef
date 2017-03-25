98932a8c26f5919924b3ac4725529a5c3bff586a
First Project <Ely.kashef@gmail.com>
2017-03-25 11:10:03 -0700

    initional

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        private static object intSum;
+
+        static void Main(string[] args)
+        {
+            int intIndex;
+            int intSum = 0;
+            for (intIndex = 0; intIndex <= 10; intIndex++)
+            {
+                intSum = intSum + intIndex;
+            }
+            string strSum = System.Convert.ToString(intSum);
+            Console.WriteLine(string.Format("sum is :{0} ", strSum));
+            System.Console.ReadLine();
+           
+        }
+    }
+}
