c264390b8593528f030d51a366a505a257990efa
First Project <Ely.kashef@gmail.com>
2017-04-06 13:00:44 -0700

    Class Stack

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -9,20 +9,18 @@ namespace ConsoleApplication1
 {
     class Program
     {
-        private static object intSum;
+        
 
         static void Main(string[] args)
         {
-            int intIndex;
-            int intSum = 0;
-            for (intIndex = 0; intIndex <= 10; intIndex++)
-            {
-                intSum = intSum + intIndex;
-            }
-            string strSum = System.Convert.ToString(intSum);
-            Console.WriteLine(string.Format("sum is :{0} ", strSum));
-            System.Console.ReadLine();
-           
+            Stacks Ostack = new Stacks();
+            Ostack.push(7);
+            Ostack.push(11);
+            int intStackData;
+            intStackData = Ostack.Pop();
+            System.Console.WriteLine(">" + intStackData);
+          
+
         }
     }
 }
---
+++ ConsoleApplication1/Stacks.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Stacks
+    {
+        private int intIndex = -1;
+        private System.Collections.ArrayList Array = new System.Collections.ArrayList();
+
+        public void push(int Data)
+        {
+            intIndex++;
+            Array.Add(Data);
+        }
+
+        public int Pop()
+        {
+            if (intIndex >= 0)
+            {
+                
+                for (intIndex = 0; intIndex < Array.Capacity - 1; intIndex++)
+                {
+                    int intData = (int)Array[intIndex];
+                    Array.RemoveAt(intIndex);
+                    return (intData);
+                }
+            }
+            else
+            {
+                return (-1);
+            }
+        }
+    }
+}
